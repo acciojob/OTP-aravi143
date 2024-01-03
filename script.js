b5714f079370368d83ad92d2ce2d3448a792c58c
Aravind Chinna <151151715+aravi143@users.noreply.github.com>
2024-01-03 11:08:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const codeInputs = document.querySelectorAll('.code');
+
+codeInputs.forEach((input, index) => {
+  input.addEventListener('input', () => {
+    if (input.value.length === 1) {
+      codeInputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener('keydown', (event) => {
+    if (event.key === 'Backspace' && input.value === '') {
+      codeInputs[index - 1].focus();
+    }
+  });
+});
